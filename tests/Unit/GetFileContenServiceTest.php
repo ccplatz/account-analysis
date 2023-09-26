28dfabdd180430402f5b1f172521ee877d37b0a7
Carsten-Christian Platz <mail@ccplatz.de>
2023-09-26 15:09:03 +0200

    fix wrong namespaces

--- tests/Unit/GetFileContenServiceTest.php
+++ tests/Unit/GetFileContenServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Models\File;
 use App\Services\GetFileContentService;
--- tests/Unit/ImportServiceTest.php
+++ tests/Unit/ImportServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Models\Account;
 use App\Models\Transaction;
